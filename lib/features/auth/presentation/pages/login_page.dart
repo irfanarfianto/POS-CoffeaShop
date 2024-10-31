abfd06f127fb4edd204e262ba912a581ef888797
irfanarfianto <irfanarfianto92@gmail.com>
2024-10-31 20:08:41 +0700

    Remove font_awesome_flutter import from login and register pages

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
 import 'package:icons_plus/icons_plus.dart';
 
